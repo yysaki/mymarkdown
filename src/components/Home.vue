9c695a2c5b2086271e7cd5689dbb1eb8d6b2ea50
yysaki <yysaki@yysaki.com>
2018-08-19 17:51:23 +0900

    chapter4.1: Add components

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <button>Googleアカウントでログイン</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Welcome to MyMarkdown"
+    };
+  }
+};
+</script>
