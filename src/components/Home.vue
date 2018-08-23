58fb8f7adb39ea42d5aaaca01d31732364f8bf27
yysaki <yysaki@yysaki.com>
2018-08-23 19:41:08 +0900

    Markup Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="home">
-    <h1>{{ msg }}</h1>
-    <button @click="googleLogin">Googleアカウントでログイン</button>
-  </div>
+  <v-containe fuild fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card id="home" class="elevation-12">
+          <v-card-text color="indigo" dark>
+            {{ msg }}
+          </v-card-text>
+          <v-card-actions>
+            <v-layout align-center justify-center>
+              <v-btn color="indigo" dark @click="googleLogin">
+                Googleアカウントでログイン
+              </v-btn>
+            </v-layout>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-containe>
 </template>
 
 <script>
