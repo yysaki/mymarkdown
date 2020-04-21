cfd3641669102cc77c21af445b6b48ae15f16d04
yysaki <mail@yysaki.com>
2020-04-21 21:15:40 +0900

    Update babel for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -44,8 +44,13 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
